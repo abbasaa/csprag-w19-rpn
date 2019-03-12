ca6aaf5c2ec9869ac40fa91a17cbf3a28c8d2c8e
Syed Abbas Ahmed <abbasaa@umich.edu>
2019-03-12 12:56:59 -0400

    changes text color based on operator

--- rpn.py
+++ rpn.py
@@ -25,7 +25,23 @@ def calculate(myarg):
             
             arg2 = stack.pop()
             arg1 = stack.pop()
-            print(stylize(arg1, colored.fg("royal_blue_1")) + " " + stylize(token, colored.fg("magenta"))+ " " + stylize(arg2, colored.fg("light_red")))
+            tokenatt = colored.fg("blue") + colored.attr("bold")
+            if (token == '-'): 
+                tokenatt = colored.fg("red") + colored.attr("bold")
+
+            if (token == '*'): 
+                tokenatt = colored.fg("medium_purple") + colored.attr("bold")
+            
+            if (token == '/'): 
+                tokenatt = colored.fg("sky_blue_1") + colored.attr("bold")  
+
+            if (token == '^'): 
+                tokenatt = colored.fg("light_pink_4") + colored.attr("bold")
+
+            if (token == '%'): 
+                tokenatt = colored.fg("dark_violet_1b") + colored.attr("bold")  
+            
+            print(stylize(arg1, colored.fg("royal_blue_1")) + " " + stylize(token, tokenatt) + " " + stylize(arg2, colored.fg("light_red")))
             result = function(arg1, arg2)
             stack.append(result)
         print(stack)
