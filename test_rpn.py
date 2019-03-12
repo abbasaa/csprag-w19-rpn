941ddb2946d1b0f739435e4d15377be593a9ec43
Syed Abbas Ahmed <abbasaa@umich.edu>
2019-03-12 02:38:28 -0400

    added new modulus operator but not test to see coverage analysis go down

--- test_rpn.py
+++ test_rpn.py
@@ -18,6 +18,3 @@ class TestBasics(unittest.TestCase):
     def test_exponent(self):
         result = rpn.calculate("2 3 ^")
         self.assertEqual(8, result)
-    def test_exponent(self):
-        result = rpn.calculate("2 3 ^")
-        self.assertEqual(8, result)
