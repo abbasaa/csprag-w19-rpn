047b93f16ab54d99951940e51847977255f471df
Syed Abbas Ahmed <abbasaa@umich.edu>
2019-03-12 00:59:17 -0400

    added stuff to travis for integration with codecov

--- test_rpn.py
+++ test_rpn.py
@@ -20,4 +20,4 @@ class TestBasics(unittest.TestCase):
         self.assertEqual(8, result)
     def test_exponent(self):
         result = rpn.calculate("2 3 ^")
-        self.assertEqual(1, result)
+        self.assertEqual(8, result)
